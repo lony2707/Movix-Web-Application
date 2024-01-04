277d6b7ba08866824a9dd97b752c44e14d4cff82
lony2707 <saloni100jha@gmail.com>
2024-01-04 15:09:06 +0530

    first commit

---
+++ src/pages/details/carousels/Recommendation.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import UseFetch from "../../../hoooks/UseFetch";
+
+const Recommendation = ({ mediaType, id }) => {
+  const { data, loading, error } = UseFetch(
+    `/${mediaType}/${id}/recommendations`
+  );
+
+  return (
+    <Carousel
+      title="Recommendations"
+      data={data?.results}
+      loading={loading}
+      endpoint={mediaType}
+    />
+  );
+};
+
+export default Recommendation;
---
+++ src/pages/details/carousels/Similar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
+import UseFetch from "../../../hoooks/UseFetch";
+
+const Similar = ({ mediaType, id }) => {
+  const { data, loading, error } = UseFetch(`/${mediaType}/${id}/similar`);
+
+  const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+
+  return (
+    <Carousel
+      title={title}
+      data={data?.results}
+      loading={loading}
+      endpoint={mediaType}
+    />
+  );
+};
+
+export default Similar;
